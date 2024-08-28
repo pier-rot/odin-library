282901f453b96de51154b097b462f72542fd901e
Pierre V <6772696d@gmail.com>
2024-08-28 15:22:12 +0200

    Add default files

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, edition) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.edition = edition;
+}
+
+function addBookToLibrary(book) {
+  // do stuff here
+}
