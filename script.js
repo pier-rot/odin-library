c985621b3cbb6e62dc546f99f6e95f84c71a0776
Pierre V <6772696d@gmail.com>
2024-09-02 16:08:16 +0200

    Make 'makeCardFromBook' function

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function Book(title, author, pages, edition, year, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  // Update HTML
 }
 
 function toggleBookRead(book) {
@@ -22,3 +23,40 @@ function showPopUp (id) {
   const popup = document.getElementById(id);
   popup.classList.toggle("show");
 }
+
+// Returns HTML Node/card from book
+function makeCardFromBook (book) {
+  const bookContainer = document.createElement("div");
+  bookContainer.setAttribute("class", "book-container");
+  const bookDiv = document.createElement("div");
+  bookDiv.setAttribute("class", "book");
+  const buttonsDiv = document.createElement("div");
+  
+  for(const [key, value] of Object.entries(book)){
+    const p = document.createElement("p");
+    p.setAttribute("class", `${key}`);
+    p.innerText = `${value}`;
+    bookDiv.appendChild(p);
+    bookDiv.dataset[key] = value;
+  }
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("id", "deleteBtn");
+  deleteBtn.setAttribute("type", "button");
+  deleteBtn.innerText = "󰛌";
+  
+  const editBtn = document.createElement("button");
+  editBtn.setAttribute("id", "editBtn");
+  editBtn.setAttribute("type", "button");
+  editBtn.innerText = "󰏫";
+
+  buttonsDiv.appendChild(deleteBtn);
+  buttonsDiv.appendChild(editBtn);
+
+  bookContainer.appendChild(bookDiv);
+  bookContainer.appendChild(buttonsDiv);
+
+  return bookContainer;
+}
+
+const lotr1 = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, 1,1954, false);
